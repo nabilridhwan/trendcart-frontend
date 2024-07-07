9da488a778b0900ad82c44f6816f7a4d5106ceb4
Nabil Ridhwan <nabridhwan@gmail.com>
2024-07-07 22:02:31 +0800

    feat: implement login redirect

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,18 +1,38 @@
 "use client";
 import React from "react";
 import { LoginBox, LoginButton, PageWrapper } from "@/app/login/login.styles";
-import Logo from "@/public/icons/tiktok.svg";
-import { handleOAuthRedirect } from "@/services/auth/tiktok-redirection-api-service";
-import { AuthAPIService } from "@/services/auth/auth-api-service";
 import { ToastContainer } from "react-toastify";
+import Logo from "@/public/icons/tiktok.svg";
+
+function generateTikTokLoginLink() {
+  const csrfState = Math.random().toString(36).substring(2);
+
+  let url = "https://www.tiktok.com/v2/auth/authorize/";
+
+  // the following params need to be in `application/x-www-form-urlencoded` format.
+  url += `?client_key=${process.env.NEXT_PUBLIC_TIKTOK_DEV_CLIENT_KEY}`;
+  url += "&scope=user.info.basic";
+  url += "&response_type=code";
+  url += "&redirect_uri=" + process.env.NEXT_PUBLIC_TIKTOK_DEV_REDIRECT_URI!;
+  url += "&state=" + csrfState;
+  // url += `&code_challenge=${CODE_VERIFIER}`;
+  // url += '&code_challenge_method=S256';
+
+  console.log(url);
+
+  // res.redirect(url);
+  return url;
+}
 
 export default function Login() {
   const handleLogin = () => {
-    const userInfo = {
-      email: "Peggie_Schoen85@gmail.com",
-      password: "ABC123213123",
-    };
-    AuthAPIService.postLogin(userInfo);
+    // const userInfo = {
+    //   email: "Peggie_Schoen85@gmail.com",
+    //   password: "ABC123213123",
+    // };
+    // AuthAPIService.postLogin(userInfo);
+
+    window.location.href = generateTikTokLoginLink();
   };
 
   return (
